619c9708a065ba675edd97e437c9b29ec828f3ac
stasmaj <s.majevski@cointraffic.io>
2019-04-19 11:51:30 +0300

    Player can pick up objects

--- Roll a Ball/Assets/PlayerController.cs
+++ Roll a Ball/Assets/PlayerController.cs
@@ -2,26 +2,33 @@
 
 namespace Assets
 {
-    public class PlayerController : MonoBehaviour
-    {
+    public class PlayerController : MonoBehaviour {
 
         public float speed;
 
         private Rigidbody rb;
 
-        void Start()
+        void Start ()
         {
             rb = GetComponent<Rigidbody>();
         }
 
-        void FixedUpdate()
+        void FixedUpdate ()
         {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+            float moveHorizontal = Input.GetAxis ("Horizontal");
+            float moveVertical = Input.GetAxis ("Vertical");
 
-            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-            rb.AddForce(movement * speed);
+            rb.AddForce (movement * speed);
+        }
+
+        void OnTriggerEnter(Collider other) 
+        {
+            if (other.gameObject.CompareTag ("Pick Up"))
+            {
+                other.gameObject.SetActive (false);
+            }
         }
     }
 }
