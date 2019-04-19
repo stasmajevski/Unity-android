489a2ddadb639db3d14f0cb2e8f87022f7fc7464
stasmaj <s.majevski@cointraffic.io>
2019-04-19 12:29:33 +0300

    Added count and win texts

--- Roll a Ball/Assets/PlayerController.cs
+++ Roll a Ball/Assets/PlayerController.cs
@@ -1,33 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets
 {
-    public class PlayerController : MonoBehaviour {
+    public class PlayerController : MonoBehaviour
+    {
 
         public float speed;
+        public Text countText;
+        public Text winText;
 
         private Rigidbody rb;
+        private int count;
 
-        void Start ()
+        void Start()
         {
             rb = GetComponent<Rigidbody>();
+            count = 0;
+            SetCountText();
+            winText.text = "";
         }
 
-        void FixedUpdate ()
+        void FixedUpdate()
         {
-            float moveHorizontal = Input.GetAxis ("Horizontal");
-            float moveVertical = Input.GetAxis ("Vertical");
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-            Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-            rb.AddForce (movement * speed);
+            rb.AddForce(movement * speed);
         }
 
-        void OnTriggerEnter(Collider other) 
+        void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.CompareTag ("Pick Up"))
+            if (other.gameObject.CompareTag("Pick Up"))
             {
-                other.gameObject.SetActive (false);
+                other.gameObject.SetActive(false);
+                count = count + 1;
+                SetCountText();
+            }
+        }
+
+        void SetCountText()
+        {
+            countText.text = "Count: " + count.ToString();
+            if (count >= 8)
+            {
+                winText.text = "You Win!";
             }
         }
     }
