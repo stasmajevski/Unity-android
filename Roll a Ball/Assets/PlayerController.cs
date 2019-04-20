7834baab4c7cdd8086fecab5a2fc812d4536d49e
stasmaj <s.majevski@cointraffic.io>
2019-04-20 12:43:07 +0300

    Changed movement to Andoird

--- Roll a Ball/Assets/PlayerController.cs
+++ Roll a Ball/Assets/PlayerController.cs
@@ -23,8 +23,8 @@ namespace Assets
 
         void FixedUpdate()
         {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+            float moveHorizontal = Input.acceleration.x;
+            float moveVertical = Input.acceleration.y;
 
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
